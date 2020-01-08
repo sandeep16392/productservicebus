e3443f63d53d53e084028a3904016835de4d3bc2
sandeep16392 <sandeepkumar16392@gmail.com>
2020-01-08 15:10:01 +0530

    Fibonacci - Telstra test

---
+++ ProductsQ.Api/Controllers/FibonacciController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductsQ.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FibonacciController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get([FromQuery]int n)
+        {
+            int a = 0, b = 1, sum=0, i;
+            for (i = 1; i < n; ++i)  
+            {
+                sum = a + b;
+                a = b;
+                b = sum;
+            }
+
+            return Ok(sum);
+        }
+    }
+}
